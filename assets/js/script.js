c58ce7698eea06fd9b962adff0f767058748469a
Joseph Preimesberger <j.preimesberger@gmail.com>
2024-01-06 16:46:42 -0600

    added page header and initial framework for hourly rows

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const today = dayjs();
+const todayFormatted = today.format('dddd, MMMM D');
+$('#todays-date').text(todayFormatted);
+
+const hoursText = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+
+function generateRows() {
+    
+}
